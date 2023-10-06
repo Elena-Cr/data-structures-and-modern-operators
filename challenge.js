1784b84e754d9b9c52dffdf0af603000f17b4dfc
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-06 20:53:32 +0200

    completed challenge

--- challenge.js
+++ challenge.js
@@ -44,4 +44,32 @@ const game = {
    const[players1,players2]=game.players;
    console.log(players1,players2);
 
- 
+   //2.
+   const[gk,...fieldPlayers]=players1;
+   console.log(gk,fieldPlayers);
+
+   //3.
+   const allPlayers=[...players1,...players2];
+   console.log(allPlayers);
+
+   //4.
+   const players1Final = [...players1,'Thiago', 'Coutinho', 'Perisic'];
+   console.log(players1Final);
+
+   //5.
+   const[team1,draw,team2]=[game.odds.team1,game.odds.x,game.odds.team2];
+   console.log(team1,draw,team2);
+
+   //6.
+   const printGoals=function(...playerName){
+    let goals = 0;
+    for(let i = 0; i<playerName.length;i++){
+        console.log(playerName[i]);
+        goals+=1;
+    }
+    console.log(`There have been ${goals} goals in this game!`);
+   }
+
+   console.log(printGoals(...game.scored));
+
+   
