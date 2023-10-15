336a60ab35eb668e7ff64c198bc7dfa5f7e65c2c
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-15 18:27:11 +0200

    Example of the fundementals of using Maps

--- script.js
+++ script.js
@@ -58,7 +58,41 @@ rest.set('name','Classico Italiano');
 rest.set(1,'Fireze,Italy');
 console.log(rest.set(2,'Lisbon,Portugal'));
 
-rest.set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open',11).set('close',23);
+//Seting multiple keys at a time 
+rest
+.set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open',11).set('close',23)
+.set(true,'We are open :D')
+.set(false,'We are closed :(');
+
+//Fetching a value using the key
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+
+//Example of using booleans as match keys
+const time = 21;
+console.log(rest.get(time>rest.get('open')&&time<rest.get('close')));
+
+//Methods
+console.log(rest.has('categories'));
+rest.delete(2);
+//rest.clear();
+console.log(rest);
+console.log(rest.size);
+
+//Using ARRAYS as match keys
+const arr =[1,2]
+rest.set(arr,'Test')
+rest.set(document.querySelector('h1'),'Heading')
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+
+
+
 
 
 // //SETS
