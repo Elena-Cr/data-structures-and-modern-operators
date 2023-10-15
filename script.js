12371132bc512274c3c06a824f1c9208d52b4b2e
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-15 19:06:03 +0200

    examples of iterating Maps and switching between types

--- script.js
+++ script.js
@@ -52,43 +52,79 @@ const restaurant = {
   },
 };
 
-//MAPS
-const rest = new Map();
-rest.set('name','Classico Italiano');
-rest.set(1,'Fireze,Italy');
-console.log(rest.set(2,'Lisbon,Portugal'));
+// MAPS:ITERATION//
 
-//Seting multiple keys at a time 
-rest
-.set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-.set('open',11).set('close',23)
-.set(true,'We are open :D')
-.set(false,'We are closed :(');
+const question = new Map ([
+  ['question','What is the best programming languare in the world?']
+  ,[1,'C']
+  ,[2,'Java']
+  ,[3,'JavaScript']
+  ,['correct',3]
+  ,[true,'Correct']
+  ,[false,'Try again!']
+]);
+console.log(question);
 
-//Fetching a value using the key
-console.log(rest.get('name'));
-console.log(rest.get(true));
+//Conver OBJECT to MAP 
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Looping MAPS
+console.log(question.get('question'));
+for(const [key,value] of question){
+  if(typeof key==='number')console.log(`Answer ${key}: ${value}`);
+};
+
+//const answer = Number(prompt('Your answer is ... '));
+const answer=3;
+//console.log(answer);
+
+console.log(question.get(answer===question.get('correct')));
+
+//Convert MAP to ARRAY 
+console.log([...question]);
+//console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+// //MAPS// //
+// const rest = new Map();
+// rest.set('name','Classico Italiano');
+// rest.set(1,'Fireze,Italy');
+// console.log(rest.set(2,'Lisbon,Portugal'));
+
+// //Seting multiple keys at a time 
+// rest
+// .set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+// .set('open',11).set('close',23)
+// .set(true,'We are open :D')
+// .set(false,'We are closed :(');
+
+// //Fetching a value using the key
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
 
 
-//Example of using booleans as match keys
-const time = 21;
-console.log(rest.get(time>rest.get('open')&&time<rest.get('close')));
+// //Example of using booleans as match keys
+// const time = 21;
+// console.log(rest.get(time>rest.get('open')&&time<rest.get('close')));
 
-//Methods
-console.log(rest.has('categories'));
-rest.delete(2);
-//rest.clear();
-console.log(rest);
-console.log(rest.size);
+// //Methods
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// //rest.clear();
+// console.log(rest);
+// console.log(rest.size);
 
-//Using ARRAYS as match keys
-const arr =[1,2]
-rest.set(arr,'Test')
-rest.set(document.querySelector('h1'),'Heading')
-console.log(rest);
-console.log(rest.size);
+// //Using ARRAYS as match keys
+// const arr =[1,2]
+// rest.set(arr,'Test')
+// rest.set(document.querySelector('h1'),'Heading')
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
 
 
