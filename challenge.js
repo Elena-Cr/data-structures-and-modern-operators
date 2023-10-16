875defdd62e482dfb552ac949c9c030ad1bf50c1
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-16 19:39:47 +0200

    Challenge 3

--- challenge.js
+++ challenge.js
@@ -40,6 +40,20 @@ const game = {
     },
     }
 
+    const gameEvents = new Map([
+        [17, '⚽ GOAL'],
+        [36, '🔁 Substitution'],
+        [47, '⚽ GOAL'],
+        [61, '🔁 Substitution'],
+        [64, '🔶 Yellow card'],
+        [69, '🔴 Red card'],
+        [70, '🔁 Substitution'],
+        [72, '🔁 Substitution'],
+        [76, '⚽ GOAL'],
+        [80, '⚽ GOAL'],
+        [92, '🔶 Yellow card'],
+        ]);
+
     //Challenge 1//
    //1.
    const[players1,players2]=game.players;
@@ -92,4 +106,23 @@ const game = {
     for (const [team,odds] of Object.entries(game.odds)){
         let teamStr = team ==='x'?'draw': `victory ${game[team]}`;
         console.log(`Odd of ${teamStr} ${odds}`);
+    }
+
+    //Challenge 3 //
+    //1.
+    const events = new Set(gameEvents.values());
+    console.log(events);
+
+    //2.
+    gameEvents.delete(64);
+    console.log(gameEvents);
+
+    //3.
+    const avgMin = 90 / gameEvents.size ;
+    console.log(`An event has happened on average, every ${avgMin} minutes`);
+
+    //4.
+    for(const [key,value] of gameEvents){
+        const half =  key<45 ? 'FIRST HALF':'SECOND HALF';
+        console.log(`[${half}] ${key} : ${value}`) ;
     }
